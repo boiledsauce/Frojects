61d58865f8a5f27457af8bf9d75b3a12328d2bfd
boiledsauce <85931509+boiledsauce@users.noreply.github.com>
2022-01-30 16:37:16 +0100

    Implemente database initialization Close #2

--- db/initialize-database.sql
+++ db/initialize-database.sql
@@ -1,4 +1,56 @@
-CREATE TABLE humans(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(20)
+CREATE TABLE User(
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Avatar VARCHAR(50)
 )
+
+CREATE TABLE Project(
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    Name VARCHAR(50),
+    OwnerId INT,
+    CreationDate datetime,
+
+    FOREIGN KEY (ownerId) REFERENCES User(Id),
+)
+
+
+CREATE TABLE UserProjectAccess(
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    UserId INT,
+    ProjectId INT,
+    InviteDate datetime,
+
+    FOREIGN KEY (UserId) REFERENCES User(Id),
+    FOREIGN KEY (ProjectId) REFERENCES Project(Id)
+)
+
+
+CREATE TABLE Task(
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    ProjectId INT, 
+    Title VARCHAR(255),
+    Description VARCHAR(255),
+    CreationDate datetime,
+
+    FOREIGN KEY (ProjectId) REFERENCES Project(Id),
+)
+
+CREATE TABLE Comment(
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    ProjectId INT, 
+    Title VARCHAR(255),
+    Description VARCHAR(255),
+    CreationDate datetime,
+
+    FOREIGN KEY (ProjectId) REFERENCES Project(Id),
+)
+
+CREATE TABLE Deadline(
+    Id INT AUTO_INCREMENT PRIMARY KEY,
+    TaskId INT, 
+    Deadline datetime,
+
+    FOREIGN KEY (TaskId) REFERENCES Task(Id),
+)
+
