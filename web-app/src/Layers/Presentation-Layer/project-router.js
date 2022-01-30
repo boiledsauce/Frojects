ba4c1c73c84528c83e6fbecb480ba3fd409306c7
boiledsauce <lowiqbro@hotmail.com>
2022-01-30 17:31:24 +0100

    fixed bugs in app.js

--- web-app/src/Layers/Presentation-Layer/project-router.js
+++ web-app/src/Layers/Presentation-Layer/project-router.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 
-router.get('/', csrfProtection, function (request, response) {
+router.get('/', function (request, response) {
     
 
 })
 
-router.get('/:id', csrfProtection, function (request, response) {
+router.get('/:id', function (request, response) {
     
 
 })
