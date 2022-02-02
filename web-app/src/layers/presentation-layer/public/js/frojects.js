f9db2dfbe3f66626969e4dc8c9092eef0f48bb66
Gustav Persson <mailtogustav7@gmail.com>
2022-02-02 17:16:36 +0100

    Made navbar look more nice

--- web-app/src/layers/presentation-layer/public/js/frojects.js
+++ web-app/src/layers/presentation-layer/public/js/frojects.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-    let showSidebar = true
-
     const sidebar = document.querySelector("#sidebar")
+
+    const toggleButton = document.querySelector("#sidebar-toggler")
+
+    toggleButton.addEventListener("click", function() {
+        if (sidebar.offsetWidth == 0) {
+            sidebar.style.width = "70%"
+            toggleButton.querySelector("i").setAttribute("class", "bi bi-x")
+        } else {
+            sidebar.style.width = "0px"
+            toggleButton.querySelector("i").setAttribute("class", "bi bi-list")
+        }
+    })
 
     adjustSidebarToNavbar()
 
@@ -13,28 +23,11 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     })
 
-    if (window.innerWidth < 992) {
-        
-        const toggleButton = document.querySelector("#sidebar-toggler")
-
-        toggleButton.addEventListener("click", function() {
-            if (sidebar.offsetWidth == 0) {
-                sidebar.style.width = "70%"
-                showSidebar = true
-            } else {
-                sidebar.style.width = "0px"
-                showSidebar = false
-            }
-        })
-        
-    }
-
     function adjustSidebarToNavbar(){
         sidebar.style.marginTop = document.querySelector("nav").offsetHeight + "px"
     }
 
     function adjustSidebarToDesktop(){
         sidebar.style.width = null
-        showSidebar = true
     }
 })
