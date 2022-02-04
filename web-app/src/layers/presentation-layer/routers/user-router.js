8e2cc7f3d92296717e409b208cfccbb9caad5648
Gustav Persson <mailtogustav7@gmail.com>
2022-02-04 14:57:14 +0100

    Added registration form. Closes #19

--- web-app/src/layers/presentation-layer/routers/user-router.js
+++ web-app/src/layers/presentation-layer/routers/user-router.js
@@ -2,8 +2,23 @@ const express = require("express")
 
 const router = express.Router({mergeParams: true})
 
-router.get('/register', async (request, response) => {
-    response.render('user/register', {layout: false})
+const userManager = require('../../business-logic-layer/user-manager')
+
+router.get('/register', (request, response) => {
+    response.render('user/register', {layout: 'empty'})
+})
+
+router.post('/register', (request, response) => {
+    const user = {
+        firstName: request.body.firstName,
+        lastName: request.body.lastName,
+        email: request.body.email,
+        password: request.body.password,
+        confirmPassword: request.body.confirmPassword
+    }
+
+    userManager.createUser(user)
+
 })
 
 module.exports = router
