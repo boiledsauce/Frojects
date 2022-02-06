c2330cac126b5e3d645f2771afa42547f124a54c
Gustav Persson <mailtogustav7@gmail.com>
2022-02-06 22:57:14 +0100

    Removed unnecessary async statement from function

--- web-app/src/layers/data-access-layer/user-repository.js
+++ web-app/src/layers/data-access-layer/user-repository.js
@@ -1,6 +1,6 @@
 const database = require('./db')
 
-exports.createUser = async (user) => {
+exports.createUser = (user) => {
 
     const query = 'INSERT INTO User (FirstName, LastName, Email, HashedPassword) VALUES (?, ?, ?, ?)'
     const values = [user.firstName, user.lastName, user.email, user.password]
