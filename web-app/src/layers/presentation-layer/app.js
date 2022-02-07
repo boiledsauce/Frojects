7026699ab18f0935713390f266fd7bf681cdacf1
boiledsauce <85931509+boiledsauce@users.noreply.github.com>
2022-02-07 12:34:38 +0100

    added redis store to docker and app.js Close #24

--- web-app/src/layers/presentation-layer/app.js
+++ web-app/src/layers/presentation-layer/app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 const path = require('path')
+const session = require('express-session')
+const RedisStore = require("connect-redis")(session)
+const { createClient } = require("redis")
+const redisClient = createClient({ legacyMode: true,  host: 'redis', port: 6379 })
+//redisClient.connect().catch(console.error)
+
 
 const projectRouter = require('./routers/project-router')
 const userRouter = require('./routers/user-router')
@@ -11,6 +17,18 @@ const app = express()
 app.engine('hbs', expressHandlebars.engine({
     extname: 'hbs'
 }))
+
+app.use(session({
+	secret: "tdffZDd>DASD",
+	store: new RedisStore({ client: redisClient }),
+	saveUninitialized: false,
+	resave: false,
+}))
+
+app.use(function (request, response, next) {
+	response.locals.session = request.session
+	next()
+})
 
 app.use(express.urlencoded({
 	extended: false
