9426e5f32f8dba454c5bfd37b135d74de5c02c1d
Gustav Persson <mailtogustav7@gmail.com>
2022-02-21 17:25:55 +0100

    Implement OAuth flow. Closes #46

--- web-app/src/layers/presentation-layers/presentation-layer-REST/REST-app.js
+++ web-app/src/layers/presentation-layers/presentation-layer-REST/REST-app.js
@@ -1,22 +1,54 @@
+const express = require('express')
+const jwt = require('jsonwebtoken')
+const path = require('path')
+
+const ACCESS_TOKEN_SECRET = 'd52b08e837b9ec2f937b734c5563daefc7a83b28fdf1864ea7f0e1c7f2c3eb6eb216fed8f06ee8fcc96f7224f1c98a61f9ebf27bc67cc09cd4452d60583e9a9f'
+
+const user = {
+	username: "Test Testsson",
+	password: "abc123"
+}
+
+function authenticateToken(request, response, next) {
+	const authHeader = request.headers['authorization']
+	const token = authHeader && authHeader.split(' ')[1]
+
+	if (token == null) return response.sendStatus(401)
+
+	jwt.verify(token, ACCESS_TOKEN_SECRET, (error, user) => {
+		if (error) response.sendStatus(403)
+
+		request.user = user
+		next()
+	})
+}
+
 module.exports = function createApp({mainRouter, mainRESTRouter}){
 
 	return {
 
 		async getApp(){
 
-			const express = require('express')
-			const path = require('path')
-			const app = express()		
+			const app = express()	
 
 			//Parse requests
-			app.use(express.urlencoded({
-				extended: false
-			}))
+			app.use(express.json())
 
 			//Public folder for static resources.
 			app.use(express.static(path.join(__dirname, 'public')))
 
-			app.use('/', mainRouter)
+			app.use('/login', (request, response) => {
+
+				//TODO Authenticate user
+
+				const accessToken = jwt.sign(user, ACCESS_TOKEN_SECRET, { expiresIn: 60 * 60 }) //1 hour
+				response.json({ accessToken })
+			})
+
+			app.use('/', authenticateToken, (request, response) => {
+				response.json(user)
+			})
+			//app.use('/', authenticateToken, mainRouter)
 
 			//404 Page not found error handler
 			app.use((request, response) => {
