90f42fcd1893d6b129c7598c1405c0053c1ce800
Gustav Persson <mailtogustav7@gmail.com>
2022-02-27 18:24:24 +0100

    Implement service health checks. Closes #54

--- web-app/src/layers/data-access-layer/db.js
+++ web-app/src/layers/data-access-layer/db.js
@@ -1,40 +1,46 @@
-const mysql = require('mysql')
+const mysql = require('mysql2/promise')
 const { initModels } = require('./models/init-models')
 
 const DB_HOST = 'database'
 const DB_PORT = 3306
-const DB_USER = 'root'
-const DB_PASSWORD = 'abc123'
+const DB_USER = 'frojects-user'
+const DB_PASSWORD = 'iWHnm16lURvL6iHfyvcK'
 const DB_NAME = 'frojects'
 
 const { Sequelize } = require('sequelize')
 
-const connection = mysql.createConnection({
-	host     : DB_HOST,
-	port	 : DB_PORT,
-	user     : DB_USER,
-	password : DB_PASSWORD,
-	database : DB_NAME
-})
-
-initialize = () => {
+initialize = async () => {
 	try{
+		const connection = await mysql.createConnection({
+			host: DB_HOST,
+			user: DB_USER,
+			password: DB_PASSWORD,
+			port: DB_PORT
+		})
 		connection.query(`CREATE DATABASE IF NOT EXISTS ${DB_NAME}`)
+
 	} catch (error) {
-		console.log(error)
+		console.error(error)
+		throw ['Kunde inte skapa databasen']
 	}
 
 }
 
-initialize()
+try{
+	initialize()
 
-const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
-	host: DB_HOST,
-	port: DB_PORT,
-	dialect: 'mysql',
-	query: {raw: true}
-})
+	const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+		host: DB_HOST,
+		port: DB_PORT,
+		dialect: 'mysql',
+		query: {raw: true}
+	})
 
-const models = initModels(sequelize)
+	const models = initModels(sequelize)
 
-module.exports = { models }
+	module.exports = { models }
+
+} catch (error) {
+	console.log(error)
+	throw ['Kunde inte etablera anslutning till databasen']
+}
